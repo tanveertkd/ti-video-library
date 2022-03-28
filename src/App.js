9936c450426a0dbd1497fae36bc1593b1805e6e7
tanveertkd <hazarikatanveer12@gmail.com>
2022-03-28 11:44:50 +0530

    init

--- src/App.js
+++ src/App.js
@@ -7,34 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} alt="mockBee logo" width="180" height="180" />
         <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
+          Welcome to <span>TI Video Library!</span>
         </h1>
         <p className="brand-description">
           Get started by editing <code>src/App.js</code>
         </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
-        </div>
       </header>
     </div>
   );
