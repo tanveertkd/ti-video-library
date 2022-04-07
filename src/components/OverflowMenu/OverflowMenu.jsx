ff6ca81867d845fd7a3e4b5ed3fb52d278459731
tanveertkd <hazarikatanveer12@gmail.com>
2022-04-07 21:17:32 +0530

    fix: final fix

--- src/components/OverflowMenu/OverflowMenu.jsx
+++ src/components/OverflowMenu/OverflowMenu.jsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import { useLike, useAuth, useWatchLater } from '../../context';
 import { useNavigate } from 'react-router-dom';
 import './OverflowMenu.css';
+import { PlaylistModal } from '../Playlist/PlaylistModal';
 
 const OverflowMenu = ({ video }) => {
     const { likedVideos, likeVideoHandler, removeFromLikes } = useLike();
-    const { watchLaterState, addToWatchLaterHandler, removeFromWatchLaterHandler } = useWatchLater();
+    const { watchLaterState, addToWatchLaterHandler, removeFromWatchLaterHandler } =
+        useWatchLater();
 
     const { auth } = useAuth();
     const navigate = useNavigate();
@@ -12,36 +15,41 @@ const OverflowMenu = ({ video }) => {
     const alreadyLiked = likedVideos.data?.find((item) => item._id === video._id);
     const alreadyInWatchLater = watchLaterState.data?.find((item) => item._id === video._id);
 
-    return (
-        <div className="overflow-menu-container">
-            <ul className="overflow-ul">
-                <li
-                    className="overflow-li"
-                    onClick={
-                        auth
-                            ? alreadyLiked
-                                ? () => removeFromLikes(video._id)
-                                : () => likeVideoHandler(video)
-                            : () => navigate('/login')
-                    }
-                >
-                    {!alreadyLiked ? 'Like' : 'Unlike'}
-                </li>
+    const [modalVisibility, toggleModalVisibility] = useState(false);
 
-                <li
-                    className="overflow-li"
-                    onClick={
-                        auth
-                            ? alreadyInWatchLater
-                                ? () => removeFromWatchLaterHandler(video._id)
-                                : () => addToWatchLaterHandler(video)
-                            : () => navigate('/')
-                    }
-                >
-                    {!alreadyInWatchLater ? 'Watch Later' : 'Remove from Watch Later'}
-                </li>
-                <li className="overflow-li">Add to playlist</li>
-            </ul>
+    const toggleModal = () => toggleModalVisibility((modalVisibility) => !modalVisibility);
+
+    return (
+        <div>
+            <div className="overflow-menu-container">
+                <ul className="overflow-ul">
+                    <li
+                        className="overflow-li"
+                        onClick={
+                            auth
+                                ? alreadyLiked
+                                    ? () => removeFromLikes(video._id)
+                                    : () => likeVideoHandler(video)
+                                : () => navigate('/login')
+                        }
+                    >
+                        {!alreadyLiked ? 'Like' : 'Unlike'}
+                    </li>
+
+                    <li
+                        className="overflow-li"
+                        onClick={
+                            auth
+                                ? alreadyInWatchLater
+                                    ? () => removeFromWatchLaterHandler(video._id)
+                                    : () => addToWatchLaterHandler(video)
+                                : () => navigate('/')
+                        }
+                    >
+                        {!alreadyInWatchLater ? 'Watch Later' : 'Remove from Watch Later'}
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
